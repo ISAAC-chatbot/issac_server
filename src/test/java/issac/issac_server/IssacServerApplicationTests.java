3c847e0f4635f14f4d23f050131d3a7af1c53e14
kkj6235 <kkj6235@gmail.com>
2024-11-29 00:37:35 +0900

    initial commit

---
+++ src/test/java/issac/issac_server/IssacServerApplicationTests.java
@@ -0,0 +1,13 @@
+package issac.issac_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssacServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
