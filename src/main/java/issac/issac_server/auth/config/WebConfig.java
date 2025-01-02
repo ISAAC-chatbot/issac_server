dae51f4d8965c230a8e43685b1cdb7f48a7dbc84
kkj6235 <kkj6235@gmail.com>
2025-01-03 02:53:32 +0900

    Refactor : Rename AuthConfig to WebConfig

--- src/main/java/issac/issac_server/auth/config/WebConfig.java
+++ src/main/java/issac/issac_server/auth/config/WebConfig.java
@@ -1,20 +1,27 @@
 package issac.issac_server.auth.config;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
-public class AuthConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
 
     private final AuthArgumentResolver authArgumentResolver;
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(authArgumentResolver);
+    }
+    @Override
+    public void addResourceHandlers(@NotNull ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/static/swagger-ui/");
     }
 }
