37a69069ba00aac005104138d9cf31fa912a4108
kkj6235 <kkj6235@gmail.com>
2025-01-08 14:41:26 +0900

    feat(post) : add author validation for post update

--- src/main/java/issac/issac_server/post/application/PostUpdater.java
+++ src/main/java/issac/issac_server/post/application/PostUpdater.java
@@ -1,6 +1,8 @@
 package issac.issac_server.post.application;
 
 import issac.issac_server.post.domain.Post;
+import issac.issac_server.post.exception.PostErrorCode;
+import issac.issac_server.post.exception.PostException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -8,7 +10,13 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class PostUpdater {
 
-    public void update(Post post, PostUpdateRequest request) {
+    public void update(Long userId, Post post, PostUpdateRequest request) {
+        validateAuthor(userId,post);
         post.update(request);
+    }
+    private void validateAuthor(Long userId, Post post) {
+        if (!post.getAuthor().getId().equals(userId)) {
+            throw new PostException(PostErrorCode.USER_IS_NOT_AUTHOR);
+        }
     }
 }
