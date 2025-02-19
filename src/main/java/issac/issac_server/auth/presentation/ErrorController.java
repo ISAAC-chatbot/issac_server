ac669f974582432e3f6ee576fd081f6819b3b58f
kkj6235 <kkj6235@gmail.com>
2025-02-19 13:04:25 +0900

    hotfix : access token validation

--- src/main/java/issac/issac_server/auth/presentation/ErrorController.java
+++ src/main/java/issac/issac_server/auth/presentation/ErrorController.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Log4j2
 public class ErrorController {
 
-    @GetMapping("/unauthorized")
+    @RequestMapping("/unauthorized")
     public ResponseEntity<ExceptionResponse> handleUnauthorized(HttpServletRequest request) {
 
         IssacException e = (IssacException) request.getAttribute("exception");
